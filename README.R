81d5ea87fe09da98dece9750cdd2181c29d8950f
Arni Magnusson <arnima@hafro.is>
2019-01-29 14:37:54 +0100

    Comment out style

--- README.R
+++ README.R
@@ -3,8 +3,10 @@
 ## To get TOC and numbered sections, just create HTML and call it .md
 rmarkdown::render("README.Rmd", output_file="README.md")
 
-## Comment out doctype and title to make GitHub Markdown happy
+## To achieve proper GitHub rendering, comment out doctype, title, and style
 md <- readLines("README.md")
 md[md=="<!DOCTYPE html>"] <- "<!-- <!DOCTYPE html> -->"
 md <- gsub("(<title>.*</title>)", "<!-- \\1 -->", md)
+md <- gsub("<style", "<!-- <style", md)
+md <- gsub("</style>", "</style> -->", md)
 writeLines(md, "README.md")
